cddfda4c11e4750bcb3d56e4efcce2e2479c3ab9
Ahmad Alshareefi <ahmad@bigalc.com>
2026-01-09 05:43:38 +0400

    Fix: Correct import paths

--- server/src/assessment/assessment.service.ts
+++ server/src/assessment/assessment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { StartAssessmentDto, SubmitAnswerDto } from './dto/assessment.dto';
 
 @Injectable()
